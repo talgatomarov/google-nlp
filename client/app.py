60e07fc1a9247806b084b66742cd3804e81e7705
talgatomarov <talgat.omarov.1999@gmail.com>
2021-03-21 08:25:58 +0600

    Implement streamlit app

---
+++ client/app.py
@@ -0,0 +1,40 @@
+import pickle
+import streamlit as st
+import pandas as pd
+from annoy import AnnoyIndex
+import time
+import numpy as np
+
+# Loading dataset
+df = pd.read_csv('../data/articles.csv')
+content = df['content']
+
+# Loading artifacts
+vectorizer = pickle.load(open("../artifacts/vectorizer.p", "rb"))
+svd = pickle.load(open("../artifacts/svd.p", "rb"))
+tfs_truncated = pickle.load(open("../artifacts/tfs_truncated.p", "rb"))
+
+annoy_idx = AnnoyIndex(512, 'angular')
+annoy_idx.load('../artifacts/index.ann')
+
+# Query
+raw_query = st.text_input("Query")
+query = [raw_query]
+
+if raw_query:
+    t1 = time.time()
+    query_tfs = vectorizer.transform(query)
+
+    if query_tfs.nnz > 0:
+        query_tfs_truncated = svd.transform(query_tfs)
+
+        idxs = annoy_idx.get_nns_by_vector(query_tfs_truncated[0], 5)
+        t2 = time.time()
+        st.write(f'Query finished in {t2 - t1} seconds')
+
+        for idx in idxs:
+            st.header(df.title[idx])
+            st.write(f'Article ID: {df.id[idx]}')
+            st.write(df.content[idx][:500])
+    else:
+        st.text("No results were found :(")
